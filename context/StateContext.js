4875df8286db17f228a227c33804a7c1703393ee
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2022-10-12 22:44:45 +0100

    first commit

---
+++ context/StateContext.js
@@ -0,0 +1,58 @@
+import React, { createContext, useContext, useState, useEffect} from 'react'
+import {toast} from 'react-hot-toast'
+
+const Context = createContext()
+
+export const StateContext =({children}) => {
+  const [showCart, setShowCart] =useState(false)
+  const [cartItems, setCartItems] =useState([])
+  const [totalPrice, setTotalPrice] = useState()
+  const [totalQuantities, setTotalQuantities] = useState(0)
+  const[qty, setQty] = useState(1)
+
+  const onAdd = (product, quantity) =>{
+    const checkProductInCart = cartItems.find((item) => item._id === product._id)
+    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price + quantity)
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
+    if(checkProductInCart){
+        const updatedCartItems = cartItems.map((cartProduct) => {
+            if(cartProduct._id === product._id)return {
+                ...cartProduct, 
+                quantity: cartProduct.quantity + quantity
+            }
+        })
+        setCartItems(updatedCartItems)
+    } else {
+        product.quantity = quantity
+        setCartItems([...cartItems, { ...product}])
+    }
+    toast.success(`${qty} ${product.name} added to the cart.`)
+  }
+  const increaseQuantity = () =>{
+    setQty((prev)=> prev + 1)
+  }
+  const decreaseQuantity = () => {
+    setQty((prev) => {
+        if(prev - 1 < 1) return 1
+      else return  prev - 1})
+  }
+  return (
+    <Context.Provider
+      value={{
+        showCart,
+        setShowCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        increaseQuantity,
+        decreaseQuantity,
+        onAdd
+      }}
+      >
+        {children}
+    </Context.Provider>
+  )
+}
+
+export const useStateContext = () => useContext(Context)
